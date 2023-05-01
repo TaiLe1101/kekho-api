484468a069a6194c32334ff619060059bfa3c957
TaiLe1101 <taic21a.th1@gmail.com>
2023-05-01 20:22:07 +0700

    update: hehe1

--- dist/routes/index.js
+++ dist/routes/index.js
@@ -6,6 +6,17 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const product_1 = __importDefault(require("./product"));
 function route(app) {
     const endPointURL = '/api';
+    app.get('/', (req, res) => {
+        const response = {
+            status: 200,
+            err: false,
+            msg: 'Get Success',
+            data: {
+                Hello: "Hello Bro ^^ Welcome to web api end point api '~/api/*'",
+            },
+        };
+        res.status(200).json(response);
+    });
     app.use(endPointURL + '/products', product_1.default);
 }
 exports.default = route;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,22 @@
-import { Express } from 'express';
+import { Express, Request, Response } from 'express';
 
 import productRouter from './product';
+import { ClientRP } from '../interfaces/components/response';
 
 function route(app: Express) {
   const endPointURL = '/api';
+
+  app.get('/', (req: Request, res: Response) => {
+    const response: ClientRP = {
+      status: 200,
+      err: false,
+      msg: 'Get Success',
+      data: {
+        Hello: "Hello Bro ^^ Welcome to web api end point api '~/api/*'",
+      },
+    };
+    res.status(200).json(response);
+  });
 
   app.use(endPointURL + '/products', productRouter);
 }
