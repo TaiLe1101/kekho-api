ee865e0fa603c35a08aeea6c6572bcbc56d91cc5
TaiLe1101 <taic21a.th1@gmail.com>
2023-05-01 22:09:24 +0700

    update: hehe1

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,21 +4,21 @@ import productRouter from './product';
 import { ClientRP } from '../interfaces/components/response';
 
 function route(app: Express) {
-  const endPointURL = '/api';
+    const endPointURL = '/api';
 
-  app.get('/', (req: Request, res: Response) => {
-    const response: ClientRP = {
-      status: 200,
-      err: false,
-      msg: 'Get Success',
-      data: {
-        Hello: "Hello Bro ^^ Welcome to web api end point api '~/api/*'",
-      },
-    };
-    res.status(200).json(response);
-  });
+    app.get('/', (req: Request, res: Response) => {
+        const response: ClientRP = {
+            status: 200,
+            err: false,
+            msg: 'Get Success',
+            data: {
+                Hello: "Hello Bro ^^ Welcome to web api end point api '~/api/*'",
+            },
+        };
+        res.status(200).json(response);
+    });
 
-  app.use(endPointURL + '/products', productRouter);
+    app.use(endPointURL + '/products', productRouter);
 }
 
 export default route;
